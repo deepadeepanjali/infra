ed26a0d517483ac03c82d509daf75c352c13cd09
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-30 16:57:47 +0530

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -1,1 +1,12 @@
+locals {
+foldername_nw = replace(var.foldername, "_", "-")
+}
 
+
+module "keyvault-secret-rbac" {
+  source = "git::https://github.com/deepadeepanjali/tfmodule-azurerm-resource-kvsecret.git?ref=main"
+  for_each = var.config
+  secretname = "secret-${local.foldername_nw}-${each.value.botname}"
+  secretvalue = "123"
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}

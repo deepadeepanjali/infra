35f595ff4f95c526b6fab1efa6ed965d19af46c6
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-30 15:48:50 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "keyvault" {
+  description = "Name of keyvault"
+  type        = string
+}
+
+variable "resourcegroup" {
+  description = "The resource group of the Key Vault"
+  type        = string
+}
+
+variable "kv_map" {
+  description = "Map of rbacc config for keyvault"
+  type = map(object({
+    object_id     = string
+    role_name    = string
+  }))
+}
+

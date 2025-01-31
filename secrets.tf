e998725be969dba2690bddb2c1b9521e506f3299
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-31 21:08:00 +0530

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -1,12 +1,28 @@
 locals {
-foldername_nw = replace(var.foldername, "_", "-")
+  foldername_nw = replace(var.foldername, "_", "-")
+}
+
+
+module "keyvault-secret" {
+  source       = "git::https://github.com/deepadeepanjali/tfmodule-azurerm-resource-kvsecret.git?ref=main"
+  for_each     = var.botconfig
+  secretname   = "secret-${local.foldername_nw}-${each.value.botname}"
+  secretvalue  = "123"
+  keyvault_id = data.azurerm_key_vault.keyvault.id
 }
 
 
 module "keyvault-secret-rbac" {
-  source = "git::https://github.com/deepadeepanjali/tfmodule-azurerm-resource-kvsecret.git?ref=main"
-  for_each = var.config
-  secretname = "secret-${local.foldername_nw}-${each.value.botname}"
-  secretvalue = "123"
-  key_vault_id = data.azurerm_key_vault.keyvault.id
+  depends_on  = [module.keyvault-secret]
+  for_each    = local.secret_rbac  # Loop over the merged secret RBAC map
+  
+  source = "git::https://github.com/deepadeepanjali/tfmodule-azurerm-resource-rbac-role-assignments?ref=main"
+
+  role_assignments = {
+    "${each.key}" = {
+      role        = each.value.role_name
+      principal_id = each.value.object_id
+      scope       = "${data.azurerm_key_vault.keyvault.id}/secrets/module.keyvault-secret[each.value.kv_map_key].name"  
+    }
+  }
 }
